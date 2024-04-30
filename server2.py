67f3201f974684e04b0e8e7657a4d575e8f7e813
elvira1212 <91843186+elvira1212@users.noreply.github.com>
2024-04-30 12:49:26 -0500

    Add files via upload

--- server2.py
+++ server2.py
@@ -2,41 +2,55 @@
 import numpy as np
 import cv2
 from ultralytics import YOLO
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, session
 import os
 import datetime
 import time
 from threading import Condition, Lock
 
 app = Flask(__name__)
+app.secret_key = 'DEVELOPMENT_KEY'
 
 dir_notifications = '/home/searchandrescue/notifications/'
 listdir_notifications = ['/home/searchandrescue/notifications/life_raft/', '/home/searchandrescue/notifications/life_ring/', '/home/searchandrescue/notifications/life_vest/']
 
-#targets = 
+targets = ['life_raft', 'life_ring', 'life_vest']
+classes = [0,1,2]
 
 os_lock = Lock()
-
+targets_lock = Lock()
 
 display_message = Condition()
 display_timestamp = Condition()
 message = ''
 timestamp = datetime.time()
+
+#def get_classes():
+    #if 'classes' not in g:
+        #g.classes = [0,1,2]
+    #return g.classes
+
+#@app.teardown_appcontext
+#def teardown_classes(exception):
+    #classes = g.pop('classes', None)
  
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form.getlist('target'))
+        selections = request.form.getlist('target')
+        session["classes"] = [eval(i) for i in selections]
+        print(selections)                    
     return render_template('index.html')
 
-def stream(cap, model):
+def stream(cap, model, target_mask):
     while cap.isOpened:
         # Capture frame-by-frame
         success, frame = cap.read()
         
         if success:
+            print(target_mask)
             # run model
-            results=model.track(frame)
+            results=model.track(frame, classes=target_mask)
         
             # get annotated frame as numpy array
             annotated_frame = results[0].plot()
@@ -93,7 +107,11 @@ def notify():
 def live_feed():
     #delay between reloads (allows camera to release)
     time.sleep(5)
-    return Response(stream(cv2.VideoCapture(0), YOLO('best.pt')),
+    target_mask = session.get("classes", None)
+    if target_mask == None:
+        target_mask = [0,1,2]
+    print(target_mask)
+    return Response(stream(cv2.VideoCapture(0), YOLO('best.pt'), target_mask),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/notifications')
